71ad0d9319b0cfb89bda2b659449dbcd7d0aa518
UO258220 <UO258220@uniovi.es>
2022-04-19 17:58:42 +0200

    No todos los poetas son buzos

--- src/codegeneration/ExecuteCGVisitor.java
+++ src/codegeneration/ExecuteCGVisitor.java
@@ -5,22 +5,31 @@ public class ExecuteCGVisitor {
     /**
      * Rules for execute code generation
      *
+     * Function definition
+     * execute[[ FuncDefinition: definition -> type ID statement* ]] =
+     *      ID <:>;
+     *      <' * Parameters:>;
+     *      //TODO
+     *
+     *
      * Read statement
-     * execute[[ ReadStatement: statement -> expression ]]
-     *      -
+     * execute[[ ReadStatement: statement -> expression ]] =
+     *      address[[ expression ]]
+     *      <in> expression.type.suffix();
+     *      <store> expression.type.suffix();
      *
      *
      * Write statement
-     * execute[[ WriteStatement: statement -> expression ]]
+     * execute[[ WriteStatement: statement -> expression ]] =
      *      value[[ expression ]]
      *      <out> expression.type.suffix();
      *
      *
      * Assignment
-     * execute[[ Assignment: statement -> expression1 expression2 ]]
+     * execute[[ Assignment: statement -> expression1 expression2 ]] =
      *      address[[ expression1 ]]
      *      value[[ expression2 ]]
-     *      //TODO
+     *      <store> expression2.type.suffix();
      */
 
 }
